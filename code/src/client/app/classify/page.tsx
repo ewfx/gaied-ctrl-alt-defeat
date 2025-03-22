2a5543093970de097db755cf1602b8eb17894863
amyyyth <amithjee@gmail.com>
2025-03-23 01:13:42 +0530

    add dock in ui and base for pages

---
+++ code/src/client/app/classify/page.tsx
@@ -0,0 +1,7 @@
+export default function Classify() {
+  return (
+    <div>
+      <p className="text-5xl font-bold m-4">Email classification done here</p>
+    </div>
+  );
+}
--- code/src/client/app/configure/page.tsx
+++ code/src/client/app/configure/page.tsx
@@ -2,7 +2,7 @@
 export default function RequestType() {
     return(
         <div>
-            Hello
-        </div>
+      <p className="text-5xl font-bold m-4">Configuration here</p>
+    </div>
     )
 }
