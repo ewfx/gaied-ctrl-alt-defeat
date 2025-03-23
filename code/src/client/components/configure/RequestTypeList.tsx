9f09946c83310e3dbe90b16bc62ecb76c87f6be7
amyyyth <amithjee@gmail.com>
2025-03-23 15:57:17 +0530

    add loading and fetch req

--- code/src/client/components/configure/RequestTypeList.tsx
+++ code/src/client/components/configure/RequestTypeList.tsx
@@ -1,4 +1,8 @@
+"use client";
+import { backend_uri } from "@/app/Config";
+import axios from "axios";
 import { Pencil, Plus, Trash } from "lucide-react";
+import { useEffect, useState } from "react";
 import {
     Accordion,
     AccordionContent,
@@ -13,49 +17,62 @@ import {
     CardHeader,
     CardTitle,
 } from "../ui/card";
+import { Skeleton } from "../ui/skeleton";
 
 export default function RequestTypeList() {
-  const mockData = [
-    {
-      _id: "67dfcad1d8ce3bea5d93ac89",
-      name: "Loan Adjustment",
-      definition:
-        "Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.Request to adjust loan terms.",
-      sub_request_types: [
-        {
-          id: "67dfcadcd8ce3bea5d93ac8a",
-          name: "Interest Rate Change",
-          definition: "Request to change interest rate.",
-          required_attributes: ["new_rate", "effective_date"],
-        },
-        {
-          id: "67dfcadfd8ce3bea5d93ac8b",
-          name: "Interest Rate Change2",
-          definition: "Request to change interest rate.",
-          required_attributes: ["new_rate", "effective_date"],
-        },
-      ],
-    },
-    {
-      _id: "67dfcaedd8ce3bea5d93ac8c",
-      name: "Loan Adjustment2",
-      definition: "Request to adjust loan terms.",
-      sub_request_types: [
-        {
-          id: "67dfcaf5d8ce3bea5d93ac8d",
-          name: "Interest Rate Change2",
-          definition: "Request to change interest rate.",
-          required_attributes: ["new_rate", "effective_date"],
-        },
-        {
-          id: "67dfcaf8d8ce3bea5d93ac8e",
-          name: "Interest Rate Change",
-          definition: "Request to change interest rate.",
-          required_attributes: ["new_rate", "effective_date"],
-        },
-      ],
-    },
-  ];
+  type SubRequestType = {
+    id: string;
+    name: string;
+    definition: string;
+    required_attributes: string[];
+  };
+
+  type RequestType = {
+    _id: string;
+    name: string;
+    definition: string;
+    sub_request_types: SubRequestType[];
+  };
+  const [data, setData] = useState<RequestType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(backend_uri + "/reqtypes/");
+      setData(response.data);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleEditRequest = (reqId: string) => {
+    console.log("editreq", reqId);
+  };
+
+  const handleDeleteRequest = (reqId: string) => {
+    console.log("deletereq", reqId);
+  };
+
+  const handleEditSubRequest = (reqId: string) => {
+    console.log("editsub", reqId);
+  };
+
+  const handleDeleteSubRequest = (reqId: string) => {
+    console.log("deletesub", reqId);
+  };
+
+  const handleAddRequest = () => {
+    console.log("addreq");
+  };
+
+  const handleAddSubRequest = (reqId: string) => {
+    console.log("addsubreq", reqId);
+  };
 
   return (
     <Card className="w-1/2 max-w-2/3 h-fit">
@@ -64,80 +81,122 @@ export default function RequestTypeList() {
         <CardDescription>
           View the different request types used for classification.
         </CardDescription>
-        <Button className="mt-4">Add a new Request type</Button>
+        <Button className="mt-4" onClick={handleAddRequest}>
+          Add a new Request type
+        </Button>
       </CardHeader>
       <CardContent>
         <div className="grid w-full items-center gap-4">
-          <Accordion type="single" collapsible className="w-full" id="reqType">
-            {mockData.map((reqType, index) => {
-              return (
-                <AccordionItem key={reqType._id} value={reqType._id} className="">
-                  <AccordionTrigger className="text-lg">
-                    {reqType.name}
-                  </AccordionTrigger>
-                  <AccordionContent className="border-l-1 pl-3">
-                    <div className="flex w-full items-center gap-4">
-                      <Button variant={"outline"} size={"default"} className="mb-4">
-                        <Pencil /> Edit
-                      </Button>
-                      <Button
-                        variant={"destructive"}
-                        size={"default"}
-                        className="mb-4"
+          {loading ? (
+            <div className="flex flex-col space-y-3 mt-3">
+            <div className="space-y-6">
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+          ) : (
+            <Accordion
+              type="single"
+              collapsible
+              className="w-full"
+              id="reqType"
+            >
+              {data.map((reqType, index) => {
+                return (
+                  <AccordionItem
+                    key={reqType._id}
+                    value={reqType._id}
+                    className=""
+                  >
+                    <AccordionTrigger className="text-lg">
+                      {reqType.name}
+                    </AccordionTrigger>
+                    <AccordionContent className="border-l-1 pl-3">
+                      <div className="flex w-full items-center gap-4">
+                        <Button
+                          variant={"outline"}
+                          size={"default"}
+                          className="mb-4"
+                          onClick={() => handleEditRequest(reqType._id)}
+                        >
+                          <Pencil /> Edit
+                        </Button>
+                        <Button
+                          variant={"destructive"}
+                          size={"default"}
+                          className="mb-4"
+                          onClick={() => handleDeleteRequest(reqType._id)}
+                        >
+                          <Trash /> Delete
+                        </Button>
+                      </div>
+                      <p className="text-lg mb-1 font-semibold">Definition:</p>
+                      <p className="">{reqType.definition}</p>
+
+                      <div className="flex justify-between items-end">
+                        <p className="text-lg mt-3 mb-1 font-semibold">
+                          Sub Request Types:
+                        </p>
+                        <Button
+                          variant={"outline"}
+                          size={"sm"}
+                          onClick={() => handleAddSubRequest(reqType._id)}
+                        >
+                          <Plus /> Add
+                        </Button>
+                      </div>
+
+                      <Accordion
+                        type="single"
+                        collapsible
+                        className="pl-6"
+                        id="reqType"
                       >
-                        <Trash /> Delete
-                      </Button>
-                    </div>
-                    <p className="text-lg mb-1 font-semibold">Definition:</p>
-                    <p className="">{reqType.definition}</p>
-
-                    <div className="flex justify-between items-end">
-                      <p className="text-lg mt-3 mb-1 font-semibold">
-                        Sub Request Types:
-                      </p>
-                      <Button variant={"outline"} size={"sm"} className="">
-                        <Plus /> Add
-                      </Button>
-                    </div>
-
-                    <Accordion
-                      type="single"
-                      collapsible
-                      className="pl-6"
-                      id="reqType"
-                    >
-                      {reqType.sub_request_types.map((subReq, index) => {
-                        return (
-                          <AccordionItem key={subReq.id} value={subReq.id}>
-                            <AccordionTrigger>{subReq.name}</AccordionTrigger>
-                            <AccordionContent className="border-l-1 pl-3">
-                              <div className="flex w-full items-center gap-4">
-                                <Button
-                                  variant={"outline"}
-                                  size={"sm"}
-                                  className="mb-4"
-                                >
-                                  <Pencil /> Edit
-                                </Button>
-                                <Button
-                                  variant={"destructive"}
-                                  size={"sm"}
-                                  className="mb-4"
-                                >
-                                  <Trash /> Delete
-                                </Button>
-                              </div>
-                              <p>{subReq.definition}</p>
-                            </AccordionContent>
-                          </AccordionItem>
-                        );
-                      })}
-                    </Accordion>
-                  </AccordionContent>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
+                        {reqType.sub_request_types.map((subReq, index) => {
+                          return (
+                            <AccordionItem key={subReq.id} value={subReq.id}>
+                              <AccordionTrigger>{subReq.name}</AccordionTrigger>
+                              <AccordionContent className="border-l-1 pl-3">
+                                <div className="flex w-full items-center gap-4">
+                                  <Button
+                                    variant={"outline"}
+                                    size={"sm"}
+                                    className="mb-4"
+                                    onClick={() =>
+                                      handleEditSubRequest(subReq.id)
+                                    }
+                                  >
+                                    <Pencil /> Edit
+                                  </Button>
+                                  <Button
+                                    variant={"destructive"}
+                                    size={"sm"}
+                                    className="mb-4"
+                                    onClick={() =>
+                                      handleDeleteSubRequest(subReq.id)
+                                    }
+                                  >
+                                    <Trash /> Delete
+                                  </Button>
+                                </div>
+                                <p>{subReq.definition}</p>
+                                <p className="mt-2 font-bold">
+                                  Required attributes:
+                                </p>
+                                {subReq.required_attributes.join(", ")}
+                              </AccordionContent>
+                            </AccordionItem>
+                          );
+                        })}
+                      </Accordion>
+                    </AccordionContent>
+                  </AccordionItem>
+                );
+              })}
+            </Accordion>
+          )}
         </div>
       </CardContent>
     </Card>
